5ca495dec3c8c5a5ea78249c5787d79b2ea5dff1
d_hand <d_hand@skbkontur.ru>
2018-06-08 10:19:14 +0500

    Mounting Lifecycle Workers

---
+++ React/mounting-lifecycle-workers/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class WorkerList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+        workers: [],
+            loading: false,
+            error: null
+        };
+    }    
+
+    componentWillMount() {
+        this.setState({loading: true});
+        this.getFakeWorkers(this.props.count).then(
+            workers => this.setState({workers, loading: false}),
+            error => this.setState({error, loading: false})
+        );
+    }
+
+    getFakeWorkers = count => new Promise((resolves, rejects) => {
+        const api = `https://api.randomuser.me/?nat=US&results=${count}`;
+        const request = new XMLHttpRequest();
+        request.open('GET', api);
+        request.onload = () => (request.status == 200) 
+            ? resolves(JSON.parse(request.response).results) 
+            : reject(Error(request.statusText));
+        request.onerror = err => rejects(err);
+        request.send();
+    });
+
+    componentWillUpdate() {
+        console.log('updating lifecycle')
+    }
+
+    render() {
+        const { workers, loading, error } = this.state
+        return (
+            <div className="worker-list">
+                {(loading) 
+                    ? <span>Loading Workers</span> 
+                    : (workers.length) 
+                        ? workers.map((user, i) => <Worker key={i} {...user} />) 
+                        : <span>0 workers loaded...</span>}
+
+                {(error) ? <p>Error Loading Workers: error</p> : ""}
+            </div>)
+    }
+}   
+
+const Worker = ({ email, picture, name, location }) =>
+    <div className="worker">
+        <img src={picture.thumbnail} alt="" />
+        <h1>{name.first} {name.last}</h1>
+        <p><a href={"mailto:" + email}>{email}</a></p>
+        <p>{location.city}, {location.state}</p>
+    </div>
+
+ReactDom.render(<WorkerList count={2}/>, document.getElementById("react-container"));
---
+++ React/mounting-lifecycle-workers/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "bundle.js"
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: ['babel-loader'],                
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },           
+        ]
+    },
+    // для того чтобы резолвились абсолютные пути
+    resolve: {
+        modules: [
+          path.resolve('./src'), 
+          path.resolve('./node_modules')
+        ]
+    },    
+    plugins: [
+        new CopyWebpackPlugin([ { from: 'src/index.html', to: 'index.html' }] , {debug: 'info'})
+    ]    
+}
